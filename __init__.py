b2fc62ce2179e369f6c88ae9effd4f589e5d4fb8
miguel <fuermiguel@yahoo.es>
2016-12-02 17:46:23 +0000

    lo mismo de antes pero pasado por el editor ninja

--- __init__.py
+++ __init__.py
@@ -3,4 +3,5 @@
 #Tienen que ir por orden alfabetico
 
 #from controllers import controllers
+from.models import balizamiento
 from models import models
--- models/__init__.py
+++ models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
+from . import balizamiento
 from . import models
