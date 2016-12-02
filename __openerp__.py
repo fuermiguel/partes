807fa88041a39499f0a0d8316bcce8deccec5cf0
miguel <fuermiguel@yahoo.es>
2016-12-02 18:21:07 +0000

    El modulo carga los modelos, faltan las vistas

--- __openerp__.py
+++ __openerp__.py
@@ -29,7 +29,7 @@
         # 'security/ir.model.access.csv',
         #'views/views.xml',
         'views/templates.xml',
-        'views/partebalizamiento'
+        'views/partebalizamiento.xml'
     ],
     # only loaded in demonstration mode
     'demo': [
--- models/balizamiento.py
+++ models/balizamiento.py
@@ -3,17 +3,17 @@ from openerp import models, fields, api
 
 class balizamiento(models.Model):
     _name = 'partes.balizamiento'
-    _inherits = {'partes_circuitos': 'id_circuitos',
-                 'partes_papis': 'id_papis',
-                 'partes_mangas': 'id_mangas',
-                 'partes_anomalias': 'id_anomalias',
-                 'partes_incidencias': 'id_incidencias'}
+    _inherits = {'partes.circuitos': 'id_circuitos',
+                 'partes.papis': 'id_papis',
+                 'partes.mangas': 'id_mangas',
+                 'partes.anomalias': 'id_anomalias',
+                 'partes.incidencias': 'id_incidencias'}
 
-    fecha = fields.date('Fecha')
-    id_circuitos = fields.Many2many('partes_circuitos', string="circuito")
-    id_papis = fields.Many2many('partes_papis', string="papis")
-    id_magas = fields.Many2many('partes_mangas', string="mangas")
-    id_anomalias = fields.Many2one('partes_anomalias', ondelete='set null',
+    #fecha = fields.date('Fecha')
+    id_circuitos = fields.Many2many('partes.circuitos', string="circuito")
+    id_papis = fields.Many2many('partes.papis', string="papis")
+    id_magas = fields.Many2many('partes.mangas', string="mangas")
+    id_anomalias = fields.Many2one('partes.anomalias', ondelete='set null',
                                      string="anomalias", index=True)
-    id_incidencias = fields.Many2one('partes_incidencias', ondelete='set null',
+    id_incidencias = fields.Many2one('partes.incidencias', ondelete='set null',
                                      string="incidencias", index=True)
